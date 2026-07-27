9cf3c7801a1eb617fbe098296d88ce860462e99c
dbarzin <dbarzin@gmail.com>
2026-07-27 19:23:23 +0200

    fix typo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -165,7 +165,7 @@ class UserController extends Controller
         $user->language = $request->input('language');
 
         // Update controls assigned to the user
-        if (Auth::User()->isAdmin() || Auth::User()->isUser()) {
+        if (Auth::user()->isAdmin() || Auth::user()->isUser()) {
             $user->lastMeasures()->sync($request->input('controls', []));
         }
 
